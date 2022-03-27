57dd4789dd17333a99d5489e76085f4a73288193
ticdevelopers <ticdevelopers01@gmail.com>
2022-03-27 13:46:41 +0100

    started a backend for the blog section using the mongoose as the database

--- client/src/Components/Blog/Blog.js
+++ client/src/Components/Blog/Blog.js
@@ -3,8 +3,10 @@ import "./Blog.scss"
 
 const Blog = () => {
   return (
-    <div className='blog'>
-        
+    <div className='blogs'>
+        <div className="blogContainer">
+            <div className="blog"></div>
+        </div>
     </div>
   )
 }
