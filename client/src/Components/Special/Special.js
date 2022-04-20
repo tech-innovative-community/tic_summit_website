cd6d03fd5a29cacad83541ff4e89c4a64162a0e6
ticdevelopers <ticdevelopers01@gmail.com>
2022-04-20 09:46:45 +0100

    updated the special page

--- client/src/Components/Special/Special.js
+++ client/src/Components/Special/Special.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Special.css";
 import specialImg from "../../Images/2.jpg";
-
+import ReactPlayer from "react-player/youtube";
 
 const Special = () => {
   return (
@@ -21,19 +21,27 @@ const Special = () => {
           <div className="specials">
             <div className="p">02</div>
             <div className="text">
-              All students have a brilliant side to them, we're looking for this and providing a platform to inspire, mentor, grow and empower them to emerge in the future as Tech entrepreneurs and leaders. 
+              All students have a brilliant side to them, we're looking for this
+              and providing a platform to inspire, mentor, grow and empower them
+              to emerge in the future as Tech entrepreneurs and leaders.
             </div>
           </div>
           <div className="specials">
             <div className="p">03</div>
             <div className="text">
-              TiC provides oppurtunities for the youths to innovate, express and showcase
-              their creativity and solve problems using technology
+              TiC provides oppurtunities for the youths to innovate, express and
+              showcase their creativity and solve problems using technology
             </div>
           </div>
         </div>
         <div className="rightSpecial">
-          <img src={specialImg} alt="Tech Innvation Challenge" />
+          <ReactPlayer
+            className="react-player"
+            width="100%"
+            height="100%"
+            url="https://youtu.be/8GbabdQT6RQ"
+          />
+          {/* <img src={specialImg} alt="Tech Innvation Challenge" /> */}
         </div>
       </div>
     </div>
